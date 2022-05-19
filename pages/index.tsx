d42e17a5cb7a235da8b8e20c79eb9fc9eb53194e
qalbeali <qalbeali8@gmail.com>
2022-05-19 01:45:48 -0700

    logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Images from '../public/images/Images'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -9,7 +9,19 @@ const Home: NextPage = () => {
         <title>TS Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='text-3xl'>
+
+      <div className='pr-4 py-3 flex justify-center items-center'>
+        <div className='px-6'>
+          <Image src={Images.next} width="170" height="170" />
+        </div> 
+        <div className='px-6 text-3xl'>
+          +
+        </div> 
+        <div className='px-6'>
+          <Image src={Images.TP} width="120" height="120" />
+        </div> 
+      </div>
+      <div className='flex justify-center items-center text-3xl '>
       Boiler Plate for TypeScript & Next Js Project <br />
       With Installed The Tailwind Css and <br /> Mui With Cutomized theme of MUI.
       Happy Coading
@@ -20,6 +32,20 @@ const Home: NextPage = () => {
       <div className='text-5xl text-bold m-4 p-5 border-dashed border-4 border-pink-500'>
         Good Luck For Your Next Project
       </div>
+
+      <div className='flex justify-center items-center'>
+        <div className='px-6'>
+          <Image src={Images.tailwind} width="170" height="170" />
+        </div> 
+        <div className='px-6 text-3xl'>
+          +
+        </div> 
+        <div className='px-6'>
+          <Image src={Images.mui} width="120" height="120" />
+        </div> 
+      </div>
+
+      
 
     </div>
   )
---
+++ public/images/Images.tsx
@@ -0,0 +1,11 @@
+import next from "./next.png";
+import TP from "./TP.png";
+import tailwind from "./tailwind.png";
+import mui from "./mui.png";
+
+export default {
+    next,
+    TP,
+    tailwind,
+    mui
+}
